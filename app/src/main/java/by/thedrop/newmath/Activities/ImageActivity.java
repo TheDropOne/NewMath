8a94eff46d0a2e906ae0d16cb4c58183775c5af6
TheDropOne <TheDropOne@gmail.com>
2016-11-27 16:36:05 +0300

    Floating action button refactored

--- app/src/main/java/by/thedrop/newmath/Activities/ImageActivity.java
+++ app/src/main/java/by/thedrop/newmath/Activities/ImageActivity.java
@@ -1,7 +1,14 @@
 package by.thedrop.newmath.Activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import by.thedrop.newmath.R;
@@ -10,6 +17,7 @@ public class ImageActivity extends AppCompatActivity {
 
     public static int imageResource;
     private ImageView mImageView;
+    private FloatingActionButton mShareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,22 @@ public class ImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image);
 
         mImageView = (ImageView) findViewById(R.id.image_activity_mainImage);
+        mShareButton = (FloatingActionButton) findViewById(R.id.fab_share);
+        final Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), imageResource);
 
         mImageView.setImageResource(imageResource);
+        mShareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+
+                shareIntent.putExtra(Intent.EXTRA_TEXT, view.getContext().getString(R.string.share_text));
+                String url = MediaStore.Images.Media.insertImage(ImageActivity.this.getContentResolver(), bitmap, "title", "description");
+                shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(url));
+                shareIntent.setType("image/text");
+                startActivity(Intent.createChooser(shareIntent,view.getContext().getString(R.string.share_using)));
+            }
+        });
     }
 }
